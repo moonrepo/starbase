c865f2477d2212d1710fdf2328f1303ba081e1cc
Miles Johnson <mileswjohnson@gmail.com>
2023-04-09 14:56:48 -0700

    Create test lib.

--- crates/test-app/src/main.rs
+++ crates/test-app/src/main.rs
@@ -2,6 +2,7 @@ use starbase::diagnose::{Diagnostic, Error, IntoDiagnostic};
 use starbase::trace::{debug, info, warn};
 use starbase::{subscriber, system, App, Emitter, Event, MainResult, State};
 use starbase_styles::{Style, Stylize};
+use starbase_utils::fs;
 use std::path::PathBuf;
 
 #[derive(Debug, Diagnostic, Error)]
@@ -30,19 +31,23 @@ async fn start_one(states: StatesMut, emitters: EmittersMut) {
     debug!("startup 1");
 }
 
-#[system]
-async fn start_two(states: States, _resources: Resources, em: EmitterMut<TestEvent>) {
-    em.on(update_event).await;
+mod sub_mod {
+    use super::*;
 
-    tokio::spawn(async move {
-        let states = states.read().await;
-        info!("startup 2");
-        let state = states.get::<TestState>();
+    #[system]
+    pub async fn start_two(states: States, _resources: Resources, em: EmitterMut<TestEvent>) {
+        em.on(update_event).await;
 
-        dbg!(state);
-    })
-    .await
-    .into_diagnostic()?;
+        tokio::spawn(async move {
+            let states = states.read().await;
+            info!("startup 2");
+            let _ = states.get::<TestState>();
+
+            // dbg!(state);
+        })
+        .await
+        .into_diagnostic()?;
+    }
 }
 
 #[system]
@@ -51,20 +56,25 @@ async fn analyze_one(state: StateMut<TestState>, em: EmitterRef<TestEvent>) {
     **state = "mutated".to_string();
 
     let event = TestEvent(50);
-    dbg!(&event);
-    let (event, _) = em.emit(event).await.unwrap();
-    dbg!(event);
+    // dbg!(&event);
+    let (_, _) = em.emit(event).await.unwrap();
+    // dbg!(event);
 }
 
 #[system]
 async fn finish(state: StateRef<TestState>) {
     info!(val = state.0, "shutdown");
-    dbg!(state);
+    // dbg!(state);
+}
+
+#[system]
+async fn create_file() {
+    test_lib::create_file()?;
 }
 
 #[system]
 async fn missing_file() {
-    starbase_utils::fs::read_file(PathBuf::from("fake.file"))?;
+    fs::read_file(PathBuf::from("fake.file"))?;
 }
 
 #[system]
@@ -87,8 +97,9 @@ async fn main() -> MainResult {
     app.shutdown(finish);
     app.analyze(analyze_one);
     app.startup(start_one);
-    app.startup(start_two);
+    app.startup(sub_mod::start_two);
     // app.execute(missing_file);
+    app.execute(create_file);
     app.execute(fail);
 
     let ctx = app.run().await?;
