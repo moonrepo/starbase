50673c67e1285053c71a8e87811d8710d88ec69e
Miles Johnson <mileswjohnson@gmail.com>
2023-05-01 11:32:39 -0700

    Add emitter.

--- crates/events/src/emitter.rs
+++ crates/events/src/emitter.rs
@@ -5,60 +5,52 @@ use std::sync::Arc;
 use tokio::sync::RwLock;
 
 pub struct Emitter<E: Event> {
-    subscribers: RwLock<Vec<BoxedSubscriber<E>>>,
+    subscribers: Arc<RwLock<Vec<BoxedSubscriber<E>>>>,
 }
 
 #[allow(clippy::new_without_default)]
 impl<E: Event + 'static> Emitter<E> {
+    /// Create a new event emitter.
     pub fn new() -> Self {
         Emitter {
-            subscribers: RwLock::new(Vec::new()),
+            subscribers: Arc::new(RwLock::new(Vec::new())),
         }
     }
 
+    /// Return a count of how many subscribers have been registered.
     pub async fn len(&self) -> usize {
         self.subscribers.read().await.len()
     }
 
+    /// Register a subscriber to receive events.
     pub async fn subscribe<L: Subscriber<E> + 'static>(&self, subscriber: L) -> &Self {
         self.subscribers.write().await.push(Box::new(subscriber));
         self
     }
 
+    /// Register a subscriber function to receive events.
     pub async fn on<L: SubscriberFunc<E> + 'static>(&self, callback: L) -> &Self {
         self.subscribe(CallbackSubscriber::new(callback, false))
             .await
     }
 
-    // pub async fn on_many<I, L>(&self, callbacks: I) -> &Self
-    // where
-    //     I: Iterator<Item = L>,
-    //     L: SubscriberFunc<E> + 'static,
-    // {
-    //     for callback in callbacks {
-    //         self.on(callback).await;
-    //     }
-
-    //     self
-    // }
-
+    /// Register a subscriber function that will unregister itself after the first
+    /// event is received. This is useful for one-time event handlers.
     pub async fn once<L: SubscriberFunc<E> + 'static>(&self, callback: L) -> &Self {
         self.subscribe(CallbackSubscriber::new(callback, true))
             .await
     }
 
-    // pub async fn once_many<I, L>(&self, callbacks: I) -> &Self
-    // where
-    //     I: Iterator<Item = L>,
-    //     L: SubscriberFunc<E> + 'static,
-    // {
-    //     for callback in callbacks {
-    //         self.once(callback).await;
-    //     }
-
-    //     self
-    // }
-
+    /// Emit the provided event to all registered subscribers. Subscribers will be
+    /// called in the order they were registered.
+    ///
+    /// If a subscriber returns `EventState::Stop`, no further subscribers will be called.
+    /// If a subscriber returns `EventState::Return`, no further subscribers will be called
+    /// and the provided value will be returned.
+    /// If a subscriber returns `EventState::Continue`, the next subscriber will be called.
+    ///
+    /// When complete, the provided event will be returned along with the value returned
+    /// by the subscriber that returned `EventState::Return`, or `None` if not occurred.
     pub async fn emit(&self, event: E) -> miette::Result<(E, Option<E::Value>)> {
         let mut result = None;
         let mut remove_indices = HashSet::new();
