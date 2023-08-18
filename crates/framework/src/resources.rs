ec098c38e72bcde4bcaae4140602ac42f3e8223f
Miles Johnson <mileswjohnson@gmail.com>
2023-08-18 14:55:56 -0700

    Add type aliases.

--- crates/framework/src/resources.rs
+++ crates/framework/src/resources.rs
@@ -4,8 +4,14 @@ use std::{
     any::{type_name, Any, TypeId},
     sync::Arc,
 };
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 create_instance_manager!(ResourceManager, ResourceInstance);
 
 pub type Resources = Arc<RwLock<ResourceManager>>;
+
+// Not used directly but provides types for #[system] macro
+pub type ResourcesRef = RwLockReadGuard<'static, ResourceManager>;
+pub type ResourcesMut = RwLockWriteGuard<'static, ResourceManager>;
+pub type ResourceRef<T> = &'static T;
+pub type ResourceMut<T> = &'static mut T;
