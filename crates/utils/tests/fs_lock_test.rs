76c2fb518e44ade821c78e4672edc63de2167baf
Miles Johnson <mileswjohnson@gmail.com>
2023-09-07 10:38:47 -0700

    Add tests.

---
+++ crates/utils/tests/fs_lock_test.rs
@@ -0,0 +1,54 @@
+use starbase_sandbox::create_empty_sandbox;
+use starbase_utils::fs;
+use std::thread;
+use std::time::Duration;
+
+mod fs_lock {
+    use super::*;
+
+    #[tokio::test]
+    async fn async_lock_directory_all_wait() {
+        let sandbox = create_empty_sandbox();
+        let dir = sandbox.path().join("dir");
+        let mut futures = vec![];
+
+        for _ in 0..10 {
+            let dir_clone = dir.clone();
+
+            futures.push(tokio::spawn(async move {
+                let lock = fs::lock_directory(dir_clone).unwrap();
+
+                tokio::time::sleep(Duration::from_secs(1)).await;
+
+                lock.unlock().unwrap();
+            }));
+        }
+
+        for future in futures {
+            future.await.unwrap();
+        }
+    }
+
+    #[test]
+    fn sync_lock_directory_all_wait() {
+        let sandbox = create_empty_sandbox();
+        let dir = sandbox.path().join("dir");
+        let mut futures = vec![];
+
+        for _ in 0..10 {
+            let dir_clone = dir.clone();
+
+            futures.push(thread::spawn(|| {
+                let lock = fs::lock_directory(dir_clone).unwrap();
+
+                thread::sleep(Duration::from_secs(1));
+
+                lock.unlock().unwrap();
+            }));
+        }
+
+        for future in futures {
+            future.join().unwrap();
+        }
+    }
+}
