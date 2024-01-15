94b9f82245bafd4897a2f785eba70215c331c8fc
Miles Johnson <mileswjohnson@gmail.com>
2024-01-14 23:08:04 -0800

    Fix tests.

--- crates/styles/tests/color_test.rs
+++ crates/styles/tests/color_test.rs
@@ -4,6 +4,7 @@ use std::env;
 #[test]
 fn replaces_tags() {
     env::set_var("FORCE_COLOR", "1");
+    env::remove_var("NO_COLOR");
 
     assert_eq!(apply_style_tags("this <file>is</file> a <caution>string <property>with</property></caution> many <success>style</success> tags!"), "this \u{1b}[38;5;36mis\u{1b}[0m a \u{1b}[38;5;208mstring \u{1b}[38;5;147mwith\u{1b}[0m\u{1b}[0m many \u{1b}[38;5;41mstyle\u{1b}[0m tags!");
 
@@ -13,6 +14,7 @@ fn replaces_tags() {
 #[test]
 fn ignores_unknown_tags() {
     env::set_var("FORCE_COLOR", "1");
+    env::remove_var("NO_COLOR");
 
     assert_eq!(
         apply_style_tags("this is <unknown>unknown</unknown>"),
