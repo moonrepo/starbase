2042b9cad52e3a46892647afec891a95d13e416b
Miles Johnson <mileswjohnson@gmail.com>
2024-03-05 16:20:06 -0800

    Update toml.

--- crates/utils/src/toml_error.rs
+++ crates/utils/src/toml_error.rs
@@ -3,41 +3,28 @@ use starbase_styles::{Style, Stylize};
 use std::path::PathBuf;
 use thiserror::Error;
 
-#[cfg(not(feature = "miette"))]
 #[derive(Error, Debug)]
+#[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 pub enum TomlError {
+    #[cfg_attr(feature = "miette", diagnostic(transparent))]
     #[error(transparent)]
     Fs(#[from] FsError),
 
-    #[error("Failed to parse TOML file {}.\n{error}", .path.style(Style::Path))]
-    ReadFile {
-        path: PathBuf,
+    #[cfg_attr(feature = "miette", diagnostic(code(toml::format)))]
+    #[error("Failed to format TOML.")]
+    Format {
+        #[source]
+        error: toml::ser::Error,
+    },
+
+    #[cfg_attr(feature = "miette", diagnostic(code(toml::parse)))]
+    #[error("Failed to parse TOML.")]
+    Parse {
         #[source]
         error: toml::de::Error,
     },
 
-    #[error("Failed to stringify TOML.\n{error}")]
-    Stringify {
-        #[source]
-        error: toml::ser::Error,
-    },
-
-    #[error("Failed to stringify TOML for file {}.\n{error}", .path.style(Style::Path))]
-    StringifyFile {
-        path: PathBuf,
-        #[source]
-        error: toml::ser::Error,
-    },
-}
-
-#[cfg(feature = "miette")]
-#[derive(Error, Debug, miette::Diagnostic)]
-pub enum TomlError {
-    #[diagnostic(transparent)]
-    #[error(transparent)]
-    Fs(#[from] FsError),
-
-    #[diagnostic(code(toml::parse_file))]
+    #[cfg_attr(feature = "miette", diagnostic(code(toml::parse_file)))]
     #[error("Failed to parse TOML file {}.", .path.style(Style::Path))]
     ReadFile {
         path: PathBuf,
@@ -45,16 +32,9 @@ pub enum TomlError {
         error: toml::de::Error,
     },
 
-    #[diagnostic(code(toml::stringify))]
-    #[error("Failed to stringify TOML.")]
-    Stringify {
-        #[source]
-        error: toml::ser::Error,
-    },
-
-    #[diagnostic(code(toml::stringify_file))]
-    #[error("Failed to stringify TOML for file {}.", .path.style(Style::Path))]
-    StringifyFile {
+    #[cfg_attr(feature = "miette", diagnostic(code(toml::format_file)))]
+    #[error("Failed to format TOML for file {}.", .path.style(Style::Path))]
+    WriteFile {
         path: PathBuf,
         #[source]
         error: toml::ser::Error,
