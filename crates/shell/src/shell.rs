133e2b8f73db709ad6576c15bd6a7f1cff24e2f0
Miles Johnson <mileswjohnson@gmail.com>
2024-04-18 13:23:29 -0700

    Add windows.

--- crates/shell/src/shell.rs
+++ crates/shell/src/shell.rs
@@ -30,7 +30,7 @@ impl ShellType {
         ]
     }
 
-    /// Return a list of shell types for the current operating system
+    /// Return a list of shell types for the current operating system.
     pub fn os_variants() -> Vec<Self> {
         #[cfg(windows)]
         {
@@ -48,28 +48,26 @@ impl ShellType {
         Self::variants()
     }
 
+    /// Detect the current shell by inspecting the `$SHELL` environment variable,
+    /// and the parent process hierarchy.
     pub fn detect() -> Option<Self> {
         Self::try_detect().ok()
     }
 
+    /// Detect the current shell by inspecting the `$SHELL` environment variable,
+    /// and the parent process hierarchy, and return an error if not detected.
     pub fn try_detect() -> Result<Self, ShellError> {
-        let env_shell = env::var_os("SHELL");
-
-        if let Some(env_value) = &env_shell {
-            // Don't error if nothing found, continue to the more
-            // advanced OS-specific detection instead
-            if let Some(shell) = parse_shell_from_path(env_value) {
-                return Ok(shell);
-            }
+        if let Ok(env_value) = env::var("SHELL") {
+            return parse_shell_from_path(&env_value).ok_or_else(|| ShellError::UnknownShell {
+                name: env_value.into(),
+            });
         }
 
-        if let Some(shell) = detect_from_env() {
+        if let Some(shell) = detect_from_os() {
             return Ok(shell);
         }
 
-        Err(ShellError::UnknownShell {
-            name: "unknown".into(),
-        })
+        Err(ShellError::CouldNotDetectShell)
     }
 
     /// Build a [`Shell`] instance from the current type.
@@ -143,17 +141,23 @@ impl TryFrom<String> for ShellType {
 }
 
 pub fn parse_shell_from_path<P: AsRef<Path>>(path: P) -> Option<ShellType> {
-    ShellType::from_str(path.as_ref().file_stem()?.to_str()?).ok()
+    // Remove trailing extensions (like `.exe`)
+    let name = path.as_ref().file_stem()?.to_str()?;
+
+    // Remove login shell leading `-`
+    ShellType::from_str(name.strip_prefix("-").unwrap_or(name)).ok()
 }
 
-fn detect_from_env() -> Option<ShellType> {
+fn detect_from_os() -> Option<ShellType> {
+    #[cfg(windows)]
+    {
+        windows::detect()
+    }
+
     #[cfg(not(windows))]
     {
         unix::detect()
     }
-
-    #[cfg(windows)]
-    None
 }
 
 #[cfg(not(windows))]
@@ -183,7 +187,7 @@ mod unix {
         match (parts.next(), parts.next()) {
             (Some(ppid), Some(comm)) => Some(ProcessStatus {
                 ppid: ppid.parse().ok(),
-                comm: comm.strip_prefix("-").unwrap_or(comm).to_owned(),
+                comm: comm.to_owned(),
             }),
             _ => None,
         }
@@ -198,13 +202,51 @@ mod unix {
                 return None;
             }
 
-            let status = detect_from_process_status(current_pid)?;
+            let Some(status) = detect_from_process_status(current_pid) else {
+                continue;
+            };
 
             if let Some(shell) = parse_shell_from_path(status.comm) {
                 return Some(shell);
             }
 
             pid = status.ppid;
+            depth += 1;
+        }
+
+        None
+    }
+}
+
+#[cfg(windows)]
+mod windows {
+    use super::*;
+    use sysinfo::{get_current_pid, System};
+
+    pub fn detect() -> Option<ShellType> {
+        let mut system = System::new();
+        let mut pid = get_current_pid().ok();
+        let mut depth = 0;
+
+        while let Some(current_pid) = pid {
+            if depth > 10 {
+                return None;
+            }
+
+            system.refresh_process(current_pid);
+
+            if let Some(process) = system.process(current_pid) {
+                pid = process.parent();
+
+                if let Some(exe_path) = process.exe() {
+                    if let Some(shell) = parse_shell_from_path(exe_path) {
+                        return Some(shell);
+                    }
+                }
+            } else {
+                pid = None;
+            }
+
             depth += 1;
         }
 
--- crates/shell/src/shell_error.rs
+++ crates/shell/src/shell_error.rs
@@ -1,10 +1,13 @@
-use starbase_styles::{Style, Stylize};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 #[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 pub enum ShellError {
+    #[cfg_attr(feature = "miette", diagnostic(code(shell::undetected)))]
+    #[error("Could not detect your terminal shell. Scanned the $SHELL environment variable and parent processes.")]
+    CouldNotDetectShell,
+
     #[cfg_attr(feature = "miette", diagnostic(code(shell::unknown)))]
-    #[error("Unknown or unsupported shell {}.", .name.style(Style::Id))]
+    #[error("Unknown or unsupported shell {name}.")]
     UnknownShell { name: String },
 }
