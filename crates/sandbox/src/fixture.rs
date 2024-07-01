1cecc02ff8c1388fa8e71574a841b1e5ff37d119
Miles Johnson <milesj@users.noreply.github.com>
2024-07-01 11:20:16 -0700

    new: Add `env` utils. (#88)
    * Add env.
    
    * Add more utils.
    
    * Update sandbox.
    
    * Fix typo.

--- crates/sandbox/src/fixture.rs
+++ crates/sandbox/src/fixture.rs
@@ -1,14 +1,13 @@
 use clean_path::Clean;
-use std::env;
+use starbase_utils::env;
 use std::path::{Path, PathBuf};
 
 /// Locate a fixture on the file system by searching up the directory tree
-/// for a "tests/__fixtures__/<fixture>" directory, starting from the current
+/// for a `tests/__fixtures__/<fixture>` directory, starting from the current
 /// Cargo project root.
 pub fn locate_fixture<T: AsRef<str>>(fixture: T) -> PathBuf {
     let fixture = fixture.as_ref();
-    let starting_dir =
-        PathBuf::from(env::var("CARGO_MANIFEST_DIR").expect("Missing CARGO_MANIFEST_DIR!"));
+    let starting_dir = env::path_var("CARGO_MANIFEST_DIR").expect("Missing CARGO_MANIFEST_DIR!");
     let mut dir: &Path = &starting_dir;
 
     loop {
---
+++ crates/utils/src/env.rs
@@ -0,0 +1,62 @@
+use std::env;
+use std::path::PathBuf;
+
+/// Return the `PATH` environment variable as a list of [`PathBuf`]s.
+#[inline]
+pub fn paths() -> Vec<PathBuf> {
+    let Some(path) = env::var_os("PATH") else {
+        return vec![];
+    };
+
+    env::split_paths(&path).collect::<Vec<_>>()
+}
+
+/// Return an environment variable as a boolean value. If the value is a `1`, `true`,
+/// `yes`, `on`, or `enable`, return true, otherwise return false for all other cases.
+pub fn bool_var(key: &str) -> bool {
+    match env::var(key) {
+        Ok(value) => {
+            let value = value.to_lowercase();
+
+            value == "1" || value == "true" || value == "yes" || value == "on" || value == "enable"
+        }
+        Err(_) => false,
+    }
+}
+
+/// Return an environment variable with a path-like value, that will be converted
+/// to an absolute [`PathBuf`]. If the path is relative, it will be prefixed with
+/// the current working directory.
+#[inline]
+pub fn path_var(key: &str) -> Option<PathBuf> {
+    match env::var(key) {
+        Ok(value) => {
+            if value.is_empty() {
+                return None;
+            }
+
+            let path = PathBuf::from(value);
+
+            Some(if path.is_absolute() {
+                path
+            } else {
+                env::current_dir()
+                    .expect("Unable to get working directory!")
+                    .join(path)
+            })
+        }
+        Err(_) => None,
+    }
+}
+
+/// Return the "home" or "root" path for a vendor-specific environment variable,
+/// like `CARGO_HOME`. If the path is relative, it will be prefixed with the current
+/// working directory. If the variable is not defined, the fallback function will
+/// be called with the home directory.
+#[inline]
+pub fn vendor_home_var<F: FnOnce(PathBuf) -> PathBuf>(key: &str, fallback: F) -> PathBuf {
+    match path_var(key) {
+        Some(path) => path,
+        None => fallback(dirs::home_dir().expect("Unable to get home directory!")),
+    }
+}
