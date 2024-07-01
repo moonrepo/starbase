83db277215c213fb08e709ac84c1968a02e1c4de
Miles Johnson <mileswjohnson@gmail.com>
2024-07-01 11:05:49 -0700

    Add more utils.

--- crates/utils/src/env.rs
+++ crates/utils/src/env.rs
@@ -1,11 +1,34 @@
 use std::env;
 use std::path::PathBuf;
 
-/// Return the "home" or "root" path for a vendor-specific environment variable,
-/// like `CARGO_HOME`. If the path is relative, will be prefixed with the current
-/// working directory.
+/// Return the `PATH` environment variable as a list of [`PathBuf`]s.
 #[inline]
-pub fn get_vendor_home(key: &str) -> Option<PathBuf> {
+pub fn paths() -> Vec<PathBuf> {
+    let Some(path) = env::var_os("PATH") else {
+        return vec![];
+    };
+
+    env::split_paths(&path).collect::<Vec<_>>()
+}
+
+/// Return an environment variable is a boolean value. If the value is a `1`, `true`,
+/// `yes`, `on`, or `enable`, return true, otherwise return false for all other cases.
+pub fn bool_var(key: &str) -> bool {
+    match env::var(key) {
+        Ok(value) => {
+            let value = value.to_lowercase();
+
+            value == "1" || value == "true" || value == "yes" || value == "on" || value == "enable"
+        }
+        Err(_) => false,
+    }
+}
+
+/// Return an environment variable with a path-like value, that will be converted
+/// to an absolute [`PathBuf`]. If the path is relative, it will be prefixed with
+/// the current working directory.
+#[inline]
+pub fn path_var(key: &str) -> Option<PathBuf> {
     match env::var(key) {
         Ok(value) => {
             let path = PathBuf::from(value);
@@ -23,12 +46,12 @@ pub fn get_vendor_home(key: &str) -> Option<PathBuf> {
 }
 
 /// Return the "home" or "root" path for a vendor-specific environment variable,
-/// like `CARGO_HOME`. If the path is relative, will be prefixed with the current
-/// working directory. If the variable is not defined, the fallback callback will
-/// be executed with the home directory.
+/// like `CARGO_HOME`. If the path is relative, it will be prefixed with the current
+/// working directory. If the variable is not defined, the fallback function will
+/// be called with the home directory.
 #[inline]
-pub fn get_vendor_home_resolved<F: FnOnce(PathBuf) -> PathBuf>(key: &str, fallback: F) -> PathBuf {
-    match get_vendor_home(key) {
+pub fn vendor_home_var<F: FnOnce(PathBuf) -> PathBuf>(key: &str, fallback: F) -> PathBuf {
+    match path_var(key) {
         Some(path) => path,
         None => fallback(dirs::home_dir().expect("Unable to get home directory!")),
     }
