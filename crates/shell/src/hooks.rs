a8b15b938eb430c5c8b0fca88115d0b72fb15d6f
Miles Johnson <mileswjohnson@gmail.com>
2024-07-02 20:57:43 -0700

    Remove render.

--- crates/shell/src/hooks.rs
+++ crates/shell/src/hooks.rs
@@ -1,5 +1,3 @@
-use crate::helpers::normalize_newlines;
-
 pub enum Hook {
     OnChangeDir { command: String, prefix: String },
 }
@@ -8,14 +6,6 @@ impl Hook {
     pub fn get_info(&self) -> &str {
         match self {
             Hook::OnChangeDir { .. } => "on change directory",
-        }
-    }
-
-    pub fn render_template(&self, template: &str) -> String {
-        match self {
-            Hook::OnChangeDir { command, prefix } => normalize_newlines(template)
-                .replace("{command}", command)
-                .replace("{prefix}", prefix),
         }
     }
 }
