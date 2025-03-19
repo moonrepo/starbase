5e3f29574c3f8a6d0707c99e273c77b6483e67eb
Miles Johnson <mileswjohnson@gmail.com>
2025-03-19 11:37:42 -0700

    Update console.

--- crates/console/src/ui.rs
+++ crates/console/src/ui.rs
@@ -1,5 +1,6 @@
 use crate::console::Console;
 use crate::reporter::Reporter;
+use crate::stream::ConsoleStream;
 use iocraft::prelude::*;
 use miette::IntoDiagnostic;
 use std::env;
@@ -11,11 +12,14 @@ fn is_forced_tty() -> bool {
     env::var("STARBASE_FORCE_TTY").is_ok()
 }
 
-impl<R: Reporter> Console<R> {
-    pub fn render<T: Component>(&self, element: Element<'_, T>) -> miette::Result<()> {
-        let is_tty = is_forced_tty() || self.out.is_terminal();
+impl ConsoleStream {
+    pub fn render<T: Component>(
+        &self,
+        element: Element<'_, T>,
+        mut theme: ConsoleTheme,
+    ) -> miette::Result<()> {
+        let is_tty = is_forced_tty() || self.is_terminal();
 
-        let mut theme = self.theme();
         theme.supports_color = env::var("NO_COLOR").is_err() && is_tty;
 
         let canvas = element! {
@@ -29,7 +33,7 @@ impl<R: Reporter> Console<R> {
             None
         });
 
-        let buffer = self.out.buffer();
+        let buffer = self.buffer();
 
         if is_tty {
             canvas.write_ansi(buffer).into_diagnostic()?;
@@ -37,7 +41,7 @@ impl<R: Reporter> Console<R> {
             canvas.write(buffer).into_diagnostic()?;
         }
 
-        self.out.flush()?;
+        self.flush()?;
 
         Ok(())
     }
@@ -45,24 +49,28 @@ impl<R: Reporter> Console<R> {
     pub async fn render_interactive<T: Component>(
         &self,
         element: Element<'_, T>,
+        theme: ConsoleTheme,
     ) -> miette::Result<()> {
-        let is_tty = is_forced_tty() || self.out.is_terminal();
+        let is_tty = is_forced_tty() || self.is_terminal();
 
         // If not a TTY, exit immediately
         if !is_tty {
             return Ok(());
         }
 
-        self.render_loop(element).await
+        self.render_loop(element, theme).await
     }
 
-    pub async fn render_loop<T: Component>(&self, element: Element<'_, T>) -> miette::Result<()> {
-        let is_tty = is_forced_tty() || self.out.is_terminal();
+    pub async fn render_loop<T: Component>(
+        &self,
+        element: Element<'_, T>,
+        mut theme: ConsoleTheme,
+    ) -> miette::Result<()> {
+        let is_tty = is_forced_tty() || self.is_terminal();
 
-        let mut theme = self.theme();
         theme.supports_color = env::var("NO_COLOR").is_err() && is_tty;
 
-        self.out.flush()?;
+        self.flush()?;
 
         element! {
             ContextProvider(value: Context::owned(theme)) {
@@ -73,8 +81,25 @@ impl<R: Reporter> Console<R> {
         .await
         .into_diagnostic()?;
 
-        self.out.flush()?;
+        self.flush()?;
 
         Ok(())
+    }
+}
+
+impl<R: Reporter> Console<R> {
+    pub fn render<T: Component>(&self, element: Element<'_, T>) -> miette::Result<()> {
+        self.out.render(element, self.theme())
+    }
+
+    pub async fn render_interactive<T: Component>(
+        &self,
+        element: Element<'_, T>,
+    ) -> miette::Result<()> {
+        self.out.render_interactive(element, self.theme()).await
+    }
+
+    pub async fn render_loop<T: Component>(&self, element: Element<'_, T>) -> miette::Result<()> {
+        self.out.render_loop(element, self.theme()).await
     }
 }
