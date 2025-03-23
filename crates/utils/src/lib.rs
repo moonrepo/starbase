20dbadd663ce73cc76b5b04036e48d8b7b65ed7e
Miles Johnson <milesj@users.noreply.github.com>
2025-03-22 18:50:58 -0700

    new: Add glob caching. (#135)
    * Add cache.
    
    * Fix lints.
    
    * Polish.

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -10,6 +10,8 @@ mod fs_lock; // Exported from fs
 #[cfg(feature = "glob")]
 /// Utilities for globbing the file system.
 pub mod glob;
+#[cfg(feature = "glob-cache")]
+mod glob_cache;
 #[cfg(feature = "glob")]
 mod glob_error;
 
--- crates/utils/tests/glob_test.rs
+++ crates/utils/tests/glob_test.rs
@@ -146,13 +146,157 @@ mod walk_files {
         let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
         let slow = walk_files(&dir, ["**/*"]).unwrap();
-        let fast = walk_files_fast(&dir, ["**/*"]).unwrap();
+        let fast = walk_fast_with_options(
+            &dir,
+            ["**/*"],
+            GlobWalkOptions {
+                only_files: true,
+                ..Default::default()
+            },
+        )
+        .unwrap();
 
         assert_eq!(slow.len(), fast.len());
 
         let slow = walk_files(&dir, ["**/*.snap"]).unwrap();
-        let fast = walk_files_fast(&dir, ["**/*.snap"]).unwrap();
+        let fast = walk_fast_with_options(
+            &dir,
+            ["**/*.snap"],
+            GlobWalkOptions {
+                only_files: true,
+                ..Default::default()
+            },
+        )
+        .unwrap();
 
         assert_eq!(slow.len(), fast.len());
+    }
+}
+
+mod partition_patterns {
+    use super::*;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn basic() {
+        let map = partition_patterns("/root", ["foo/*", "foo/bar/*.txt", "baz/**/*"]);
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([
+                ("/root/foo".into(), vec!["*".into(), "bar/*.txt".into()]),
+                ("/root/baz".into(), vec!["**/*".into()]),
+            ])
+        );
+    }
+
+    #[test]
+    fn no_globs() {
+        let map = partition_patterns("/root", ["foo/file.txt", "foo/bar/file.txt", "file.txt"]);
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([
+                ("/root".into(), vec!["file.txt".into()]),
+                (
+                    "/root/foo".into(),
+                    vec!["file.txt".into(), "bar/file.txt".into()]
+                ),
+            ])
+        );
+    }
+
+    #[test]
+    fn same_root_dir() {
+        let map = partition_patterns("/root", ["file.txt", "file.*", "*.{md,mdx}"]);
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([(
+                "/root".into(),
+                vec!["file.*".into(), "file.txt".into(), "*.{md,mdx}".into()]
+            ),])
+        );
+    }
+
+    #[test]
+    fn same_nested_dir() {
+        let map = partition_patterns(
+            "/root",
+            ["nes/ted/file.txt", "nes/ted/file.*", "nes/ted/*.{md,mdx}"],
+        );
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([(
+                "/root/nes/ted".into(),
+                vec!["file.*".into(), "file.txt".into(), "*.{md,mdx}".into()]
+            ),])
+        );
+    }
+
+    #[test]
+    fn dot_dir() {
+        let map = partition_patterns("/root", [".dir/**/*.yml"]);
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([("/root/.dir".into(), vec!["**/*.yml".into()]),])
+        );
+    }
+
+    #[test]
+    fn with_negations() {
+        let map = partition_patterns(
+            "/root",
+            [
+                "./packages/*",
+                "!packages/cli",
+                "!packages/core-*",
+                "website",
+            ],
+        );
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([
+                ("/root".into(), vec!["website".into()]),
+                (
+                    "/root/packages".into(),
+                    vec!["*".into(), "!cli".into(), "!core-*".into()]
+                ),
+            ])
+        );
+    }
+
+    #[test]
+    fn global_negations() {
+        let map = partition_patterns(
+            "/root",
+            [
+                "foo/file.txt",
+                "foo/bar/file.txt",
+                "file.txt",
+                "!**/node_modules/**",
+            ],
+        );
+
+        assert_eq!(
+            map,
+            BTreeMap::from_iter([
+                (
+                    "/root".into(),
+                    vec!["file.txt".into(), "!**/node_modules/**".into(),]
+                ),
+                (
+                    "/root/foo".into(),
+                    vec![
+                        "file.txt".into(),
+                        "bar/file.txt".into(),
+                        "!**/node_modules/**".into(),
+                    ]
+                ),
+            ])
+        );
     }
 }
