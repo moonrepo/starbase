f8aec2d6bf88b7d56d05385fc8ad1da6ed535231
Miles Johnson <milesj@users.noreply.github.com>
2025-03-27 09:31:50 -0700

    Update iocraft. (#136)

--- crates/console/src/components/styled_text.rs
+++ crates/console/src/components/styled_text.rs
@@ -1,5 +1,4 @@
 use crate::ui::ConsoleTheme;
-use crossterm::style::{Stylize, style as paint};
 use iocraft::prelude::*;
 use starbase_styles::color::parse_tags;
 
@@ -23,11 +22,14 @@ pub fn StyledText<'a>(
 ) -> impl Into<AnyElement<'a>> + use<'a> {
     let theme = hooks.use_context::<ConsoleTheme>();
 
-    let content = parse_tags(&props.content, false)
+    let contents = parse_tags(&props.content, false)
         .into_iter()
         .map(|(text, tag)| {
-            let color = if theme.supports_color {
-                tag.as_ref()
+            let mut content = MixedTextContent::new(text);
+
+            if theme.supports_color {
+                if let Some(color) = tag
+                    .as_ref()
                     .and_then(|tag| theme.tag_to_color(tag))
                     .or_else(|| {
                         props
@@ -36,49 +38,24 @@ pub fn StyledText<'a>(
                             .and_then(|style| theme.style_to_color(style))
                     })
                     .or(props.color)
-            } else {
-                None
-            };
-
-            match color {
-                Some(color) => paint(text).with(color).to_string(),
-                None => text,
+                {
+                    content = content.color(color);
+                }
             }
+
+            if props.weight != Weight::Normal {
+                content = content.weight(props.weight);
+            }
+
+            if props.decoration != TextDecoration::None {
+                content = content.decoration(props.decoration);
+            }
+
+            content
         })
-        .collect::<Vec<_>>()
-        .join("");
+        .collect::<Vec<_>>();
 
     element! {
-        Text(
-            content,
-            weight: props.weight,
-            wrap: props.wrap,
-            align: props.align,
-            decoration: props.decoration
-        )
+        MixedText(contents, wrap: props.wrap, align: props.align)
     }
-
-    // element! {
-    //     View(flex_wrap: FlexWrap::Wrap) {
-    //         #(parts.into_iter().map(|(text, tag)| {
-    //             element! {
-    //                 Text(
-    //                     color: if theme.supports_color {
-    //                         tag.as_ref()
-    //                             .and_then(|tag| theme.tag_to_color(tag))
-    //                             .or_else(|| props.style.as_ref().and_then(|style| theme.style_to_color(style)))
-    //                             .or(props.color)
-    //                     } else {
-    //                         None
-    //                     },
-    //                     content: text,
-    //                     weight: props.weight,
-    //                     wrap: props.wrap,
-    //                     align: props.align,
-    //                     decoration: props.decoration
-    //                 )
-    //             }
-    //         }))
-    //     }
-    // }
 }
