7b506ebc70217738a69d084eafe59738585826a4
Miles Johnson <mileswjohnson@gmail.com>
2025-09-11 12:06:33 -0700

    new: Improve ID.

--- crates/id/src/lib.rs
+++ crates/id/src/lib.rs
@@ -1,6 +1,7 @@
 use compact_str::CompactString;
 use serde::{Deserialize, Serialize};
 use std::borrow::{Borrow, Cow};
+use std::ffi::{OsStr, OsString};
 use std::fmt;
 use std::ops::Deref;
 
@@ -9,15 +10,61 @@ use std::ops::Deref;
 pub struct Id(CompactString);
 
 impl Id {
+    /// Create a new identifier was the provided string as-is.
     pub fn raw<S: AsRef<str>>(id: S) -> Id {
         Id(CompactString::new(id))
     }
 
-    pub fn as_str(&self) -> &str {
+    /// Convert the identifier into an environment variable name,
+    /// by persisting alphanumeric characters and underscores,
+    /// converting dashes to underscores, and removing everything else.
+    pub fn into_env_var(self) -> String {
+        self.to_env_var()
+    }
+
+    /// Convert the identifier into an [`OsString`].
+    pub fn into_os_string(self) -> OsString {
+        self.to_os_string()
+    }
+
+    /// Convert the identifier to an environment variable name,
+    /// by persisting alphanumeric characters and underscores,
+    /// converting dashes to underscores, and removing everything else.
+    pub fn to_env_var(&self) -> String {
+        let mut var = String::new();
+
+        for ch in self.0.as_str().chars() {
+            match ch {
+                'a'..='z' | 'A'..='Z' | '0'..='9' | '_' => {
+                    var.push(ch);
+                }
+                '-' => {
+                    var.push('_');
+                }
+                _ => {}
+            }
+        }
+
+        var.to_uppercase()
+    }
+
+    /// Convert the identifier to an [`OsString`].
+    pub fn to_os_string(&self) -> OsString {
+        OsString::from(self.to_string())
+    }
+
+    /// Return the identifier as a [`CompactString`] reference.
+    pub fn as_compact_str(&self) -> &CompactString {
         &self.0
     }
 
-    pub fn as_compact_str(&self) -> &CompactString {
+    /// Return the identifier as an [`OsStr`] reference.
+    pub fn as_os_str(&self) -> &OsStr {
+        OsStr::new(&self.0)
+    }
+
+    /// Return the identifier as a [`str`] reference.
+    pub fn as_str(&self) -> &str {
         &self.0
     }
 }
@@ -42,15 +89,21 @@ impl Deref for Id {
     }
 }
 
+impl AsRef<Id> for Id {
+    fn as_ref(&self) -> &Id {
+        self
+    }
+}
+
 impl AsRef<str> for Id {
     fn as_ref(&self) -> &str {
         &self.0
     }
 }
 
-impl AsRef<Id> for Id {
-    fn as_ref(&self) -> &Id {
-        self
+impl AsRef<OsStr> for Id {
+    fn as_ref(&self) -> &OsStr {
+        OsStr::new(&self.0)
     }
 }
 
@@ -60,7 +113,20 @@ impl Borrow<str> for Id {
     }
 }
 
+impl Borrow<OsStr> for Id {
+    fn borrow(&self) -> &OsStr {
+        OsStr::new(&self.0)
+    }
+}
+
 macro_rules! gen_partial_eq {
+    (os, $ty:ty) => {
+        impl PartialEq<$ty> for Id {
+            fn eq(&self, other: &$ty) -> bool {
+                self.as_os_str() == other.as_os_str()
+            }
+        }
+    };
     ($ty:ty) => {
         impl PartialEq<$ty> for Id {
             fn eq(&self, other: &$ty) -> bool {
@@ -78,8 +144,23 @@ gen_partial_eq!(Cow<'_, str>);
 gen_partial_eq!(&Cow<'_, str>);
 gen_partial_eq!(Box<str>);
 gen_partial_eq!(&Box<str>);
+gen_partial_eq!(os, OsString);
+gen_partial_eq!(os, &OsString);
+
+impl PartialEq<OsStr> for Id {
+    fn eq(&self, other: &OsStr) -> bool {
+        self.as_os_str() == other
+    }
+}
 
 macro_rules! gen_from {
+    (os, $ty:ty) => {
+        impl From<$ty> for Id {
+            fn from(value: $ty) -> Self {
+                Self::raw(value.to_string_lossy())
+            }
+        }
+    };
     ($ty:ty) => {
         impl From<$ty> for Id {
             fn from(value: $ty) -> Self {
@@ -96,6 +177,9 @@ gen_from!(Cow<'_, str>);
 gen_from!(&Cow<'_, str>);
 gen_from!(Box<str>);
 gen_from!(&Box<str>);
+gen_from!(os, &OsStr);
+gen_from!(os, OsString);
+gen_from!(os, &OsString);
 
 #[cfg(feature = "schematic")]
 impl schematic::Schematic for Id {
