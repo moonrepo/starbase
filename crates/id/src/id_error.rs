ab6751f1ecd0291bd62d9b76290156853e88ab1d
Miles Johnson <mileswjohnson@gmail.com>
2025-09-21 16:34:04 -0700

    fix: Improve error message.

--- crates/id/src/id_error.rs
+++ crates/id/src/id_error.rs
@@ -2,7 +2,7 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 #[error(
-    "Invalid identifier format for {0}. May only contain alpha-numeric characters, dashes (-), slashes (/), underscores (_), and periods (.)."
+    "Invalid identifier format for `{0}`. May only contain alpha-numeric characters, dashes (-), slashes (/), underscores (_), and periods (.)."
 )]
 #[cfg_attr(feature = "miette", derive(miette::Diagnostic))]
 #[cfg_attr(feature = "miette", diagnostic(code(id::invalid_format)))]
